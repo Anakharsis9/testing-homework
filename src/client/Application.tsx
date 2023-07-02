9f4d52a3d8ef608188e8f85098dbd5e5ee91fd97
Dayana Ismailova <DIs@ikod.kz>
2023-07-02 12:34:40 +0600

    test: add for header

--- src/client/Application.tsx
+++ src/client/Application.tsx
@@ -34,18 +34,18 @@ export const Application: FC = () => {
 
     return <div className={bem()}>
         <Helmet titleTemplate="%s — Example store" />
-        <nav className="navbar navbar-expand-sm navbar-light bg-light">
+        <nav data-testid="header" className="navbar navbar-expand-sm navbar-light bg-light">
             <div className="container">
-                <Link className={bem('Brand', ['navbar-brand'])} to="/">Example store</Link>
-                <button className={bem('Toggler', ['navbar-toggler'])} aria-label="Toggle navigation" onClick={toggle}>
+                <Link data-testid="main" className={bem('Brand', ['navbar-brand'])} to="/">Example store</Link>
+                <button data-testid="hamburger" className={bem('Toggler', ['navbar-toggler'])} aria-label="Toggle navigation" onClick={toggle}>
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className={bem('Menu', [navbarClass])}>
                     <div className="navbar-nav">
-                        <NavLink className="nav-link" activeClassName="active" to="/catalog" onClick={hide}>Catalog</NavLink>
-                        <NavLink className="nav-link" activeClassName="active" to="/delivery" onClick={hide}>Delivery</NavLink>
-                        <NavLink className="nav-link" activeClassName="active" to="/contacts" onClick={hide}>Contacts</NavLink>
-                        <NavLink className="nav-link" activeClassName="active" to="/cart" onClick={hide}>{cartLabel}</NavLink>
+                        <NavLink data-testid="catalog" className="nav-link" activeClassName="active" to="/catalog" onClick={hide}>Catalog</NavLink>
+                        <NavLink data-testid="delivery" className="nav-link" activeClassName="active" to="/delivery" onClick={hide}>Delivery</NavLink>
+                        <NavLink data-testid="contacts" className="nav-link" activeClassName="active" to="/contacts" onClick={hide}>Contacts</NavLink>
+                        <NavLink data-testid="cart" className="nav-link" activeClassName="active" to="/cart" onClick={hide}>{cartLabel}</NavLink>
                     </div>
                 </div>
             </div>
---
+++ test/unit/header.test.tsx
@@ -0,0 +1,31 @@
+import { it, expect } from "@jest/globals";
+import { screen, fireEvent } from "@testing-library/react";
+import events from "@testing-library/user-event";
+
+import { renderApp } from "../helpers/renderApp";
+
+describe("В шапке", () => {
+  const basename = "/hw/store";
+
+  it("отображается ссылка на главную", () => {
+    const { getByTestId } = renderApp(basename);
+    expect(getByTestId("main").textContent).toEqual("Example store");
+  });
+  it("отображается ссылка на каталог", () => {
+    const { getByTestId } = renderApp(basename);
+    expect(getByTestId("catalog").textContent).toEqual('Catalog');
+  });
+
+  it("отображается ссылка на доставку", () => {
+    const { getByTestId } = renderApp(basename);
+    expect(getByTestId("delivery").textContent).toEqual('Delivery');
+  });
+  it("отображается ссылка на контакты", () => {
+    const { getByTestId } = renderApp(basename);
+    expect(getByTestId("contacts").textContent).toEqual('Contacts');
+  });
+  it("отображается ссылка на корзину", () => {
+    const { getByTestId } = renderApp(basename);
+    expect(getByTestId("cart").textContent).toEqual("Cart");
+  });
+});
