f2f0eb97944973604e921481d9ff9c4743cef553
Dayana Ismailova <DIs@ikod.kz>
2023-07-02 23:07:59 +0600

    refactor: mocks

---
+++ test/mocks/index.js
@@ -0,0 +1,26 @@
+const mockedProducts = [
+  {
+    id: 0,
+    name: "Practical Salad",
+    price: 283
+  },
+  {
+    id: 1,
+    name: "Generic Bike",
+    price: 484
+  },
+  {
+    id: 2,
+    name: "Licensed Towels",
+    price: 281
+  },
+  {
+    id: 3,
+    name: "Fantastic Tuna",
+    price: 734
+  }
+];
+
+module.exports = {
+  mockedProducts
+};
